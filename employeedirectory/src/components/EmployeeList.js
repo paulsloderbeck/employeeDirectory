04364906187afec84b142f39084ac8bc9e9b560d
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-09-22 21:52:10 -0400

    fixed table header issue

--- employeedirectory/src/components/EmployeeList.js
+++ employeedirectory/src/components/EmployeeList.js
@@ -1,24 +1,15 @@
 import React from "react";
 
 function EmployeeList(props) {
-  return (
-    <tr>
-      {props.results.map((result) => (
-        <tr key={result.index}>
-          <td>{result.index}</td>
-          <td>{result.name.first}</td>
-          <td>{result.name.last}</td>
-          <td>{result.email}</td>
-          <td>{result.dob.age}</td>
-          <img
-            alt="person"
-            src={result.picture.thumbnail}
-            className="img-fluid"
-          />
-        </tr>
-      ))}
+  return props.results.map((result) => (
+    <tr key={result.id.value}>
+      <td>{result.name.first}</td>
+      <td>{result.name.last}</td>
+      <td>{result.email}</td>
+      <td>{result.dob.age}</td>
+      <img alt="person" src={result.picture.thumbnail} className="img-fluid" />
     </tr>
-  );
+  ));
 }
 
 export default EmployeeList;
